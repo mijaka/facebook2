5d1a577db7f2282b8e72724da128c2ce50715bb6
Mia <solomianb@gmail.com>
2020-12-23 14:12:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ let database = [
 {
 	username:"mia",
 	password:"iamthebest"
+},
+{
+	username:"olive",
+	password:"123"
+},
+{
+	username:"car",
+	password:"345"
 }
 ];
 let newsFeed=[
@@ -19,9 +27,18 @@ let userNamePrompt = prompt("What`s your username?");
 let passwordPrompt = prompt("What`s your password?");
 
 function signIN(user, pass){
-	if (user === database[0].username && pass === database[1].password){
-		console.log(newsFeed);
-	} else {
-		alert("sorry, you are not a person");
+	for (let i=0; i< database.length; i++){
+		if (database[i].username === username && database[i].password === password){
+			console.log(newsFeed);
+		} else {
+			alert("sorry, we don`t know you");
+		}
 	}
+
+
+	// if (user === database[0].username && pass === database[1].password){
+	//	console.log(newsFeed);
+	// } else {
+	//	alert("sorry, you are not a person");
+	// }
 }
